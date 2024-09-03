6b2337a3814df88cc9c696a9080a831868ccd986
Bagas Setiaji <stjbagas28@gmail.com>
2024-09-04 00:18:37 +0700

    update

--- server/psgre.py
+++ server/psgre.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import load_dotenv
 import json
 import os
+from datetime import datetime, timedelta
 
 # Load the .env file
 load_dotenv()
@@ -22,15 +23,15 @@ last_value = None
 def check_for_update():
     global last_value
     with connection.cursor() as cursor:
-        cursor.execute('SELECT no_antrian FROM public."SampleDataNomorAntrian" ORDER BY id DESC LIMIT 1')
+        cursor.execute('SELECT no_antrian, nomor_hp, date FROM public."SampleDataNomorAntrian" ORDER BY id DESC LIMIT 1')
         result = cursor.fetchone()
 
         # Cek apakah ada perubahan pada kolom yang dipantau
         if result and result[0] != last_value:
             last_value = result[0]
-            send_message(last_value)
+            send_message(result[0], result[1], result[2])
 
-def send_message(value):
+def send_message(value, nomor_hp, date):
     url = 'https://api.fonnte.com/send'
     token = os.getenv('FONNTE_TOKEN')  # Get token from environment variable
     
@@ -39,9 +40,19 @@ def send_message(value):
         'Content-Type': 'application/json',
     }
     
+    # Convert date string to Unix timestamp and subtract 10 minutes
+    try:
+        date_obj = datetime.strptime(date, "%d-%m-%Y %H:%M")
+        schedule_time = date_obj - timedelta(minutes=10)
+        schedule_timestamp = int(schedule_time.timestamp())
+    except ValueError:
+        print(f"Error: Invalid date format - {date}")
+        schedule_timestamp = None
+    
     payload = {
-        'target': '081334319568',  # Replace with the destination number
-        'message': f'Column updated with new value: {value}',
+        'target': nomor_hp,  # Menggunakan nomor_hp dari database
+        'message': f'Jadwal Cek Rutin Dan Konsultasi Dokter Di Rumah Sakit Kami Adalah Pada Tanggal {date} , jangan lupa datang tepat waktu',
+        'schedule': schedule_timestamp  # Menggunakan Unix timestamp
     }
     
     response = requests.post(url, headers=headers, data=json.dumps(payload))
